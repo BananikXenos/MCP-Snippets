7438c272748b049a11fa520282b43bfef0ebd456
BananikXenos <gamerix2006@gmail.com>
2021-10-27 10:21:23 +0200

    Update Rounded Rectangle

---
+++ RoundedRectangle/README.md
@@ -0,0 +1,22 @@
+# Rounded Rectangle
+
+Draws rectangle on Screen with custom radius and Color.
+
+## Installation
+
+Just download or copy the file and you are good to go.
+
+## Usage
+
+```java
+import java.awt.Color;
+
+//Arguments: (positionX, positionY, positionX2, Rounding Amount, Color in RGB)
+RoundedRect.drawRoundedRect(0,0,150,50,10,new Color(255,255,255,255));
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- RoundedRectangle/RoundedRect.java
+++ RoundedRectangle/RoundedRect.java
@@ -1,4 +1,15 @@
-public static void drawRoundedRect(double x, double y, double x1, double y1, double radius, int color) {
+import org.lwjgl.opengl.GL11;
+
+public class RoundedRect {
+    public static void setColor(int color) {
+        float a = (color >> 24 & 0xFF) / 255.0F;
+        float r = (color >> 16 & 0xFF) / 255.0F;
+        float g = (color >> 8 & 0xFF) / 255.0F;
+        float b = (color & 0xFF) / 255.0F;
+        GL11.glColor4f(r, g, b, a);
+    }
+
+    public static void drawRoundedRect(double x, double y, double x1, double y1, double radius, int color) {
         GL11.glPushAttrib(0);
         GL11.glScaled(0.5D, 0.5D, 0.5D);
         x *= 2.0D;
@@ -20,10 +31,10 @@ public static void drawRoundedRect(double x, double y, double x1, double y1, dou
             GL11.glVertex2d(x1 - radius + Math.sin(i * Math.PI / 180.0D) * radius, y1 - radius + Math.cos(i * Math.PI / 180.0D) * radius);
         }
         for (i = 90; i <= 180; i += 3) {
-             GL11.glVertex2d(x1 - radius + Math.sin(i * Math.PI / 180.0D) * radius, y + radius + Math.cos(i * Math.PI / 180.0D) * radius);
+            GL11.glVertex2d(x1 - radius + Math.sin(i * Math.PI / 180.0D) * radius, y + radius + Math.cos(i * Math.PI / 180.0D) * radius);
         }
 
-         GL11.glEnd();
+        GL11.glEnd();
 
         GL11.glEnable(3553);
         GL11.glDisable(3042);
@@ -35,10 +46,4 @@ public static void drawRoundedRect(double x, double y, double x1, double y1, dou
 
         GL11.glPopAttrib();
     }
-public static void setColor(int color) {
-        float a = (color >> 24 & 0xFF) / 255.0F;
-        float r = (color >> 16 & 0xFF) / 255.0F;
-        float g = (color >> 8 & 0xFF) / 255.0F;
-        float b = (color & 0xFF) / 255.0F;
-        GL11.glColor4f(r, g, b, a);
-    }
+}
